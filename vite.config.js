5d8c0ea6555872b8a9eba79d23ddcd8c65fd84bf
aayushkumar1701 <aayushks1701@gmail.com>
2025-04-18 01:57:54 +0530

    added base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AnimeRadar/', // This is the path to your GitHub Pages site
 })
